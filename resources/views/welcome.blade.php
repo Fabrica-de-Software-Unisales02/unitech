700d31aa7dec0f6ef750c06fa2fff7c63c536429
izaelgs <izael-123@outlook.com>
2022-11-15 10:43:27 -0300

    volta e exibicao de status de producao

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,68 +5,7 @@
     <div class="toast-container position-fixed bottom-0 right-0 p-3">
     </div>
     <section class="institucional mb-2 mt-4 row" id="sobre">
-        <p class="mt-4">
-            Bem vindo ao Website do laboratório específico dos cursos de Análise e Desenvolvimento de Sistemas, Sistemas de Informação e Engenharia de Software do Unisales.
-            Aqui você encontrará informações sobre nossos serviços e projetos em andamento. Há também um área específica para contatos, caso queira participar de projetos,
-            sugerir um novo projeto ou até contribuir conosco de outra forma. Entre em contato!
-        </p>
-        <p>
-            Como o Laboratório faz parte dos cursos de Tecnologia da Informação e Computação do Unisales - Centro Universitário Salesiano é importante listar algumas áreas onde podemos atuar e contribuir:
-        </p>
-        <ul>
-            <li>Desenvolvimento de Sistemas</li>
-            <li>Desenvolvimento para Web e Web Mobile</li>
-            <li>Desenvolvimento Mobile</li>
-            <li>Análise de Dados / Data Science</li>
-            <li>Políticas de Segurança</li>
-            <li>Melhoria de Processos</li>
-            <li>Cursos para a comunidade</li>
-            <li>Apoio em ideação e organização de projetos em TI</li>
-
-        </ul>
-
-        <p>
-            Caso não consiga identificar nestas áreas, como podemos ajudar, entre em contato no e-mail: unitech@salesiano.br
-            (inserir fotos de laboratórios, logotipo dos softwares e apps, entre outros elementos visuais).
-        </p>
-
-        <h4>Nossos cursos:</h4>
-
-        <ul>
-            <li>Análise e Desenvolvimento de Sistemas (Tecnólogo)</li>
-            <li>Banco de Dados (Tecnólogo)</li>
-            <li>Desenvolvimento de Jogos (Tecnólogo)</li>
-            <li>Engenharia de Software (Bacharelado)</li>
-            <li>Sistemas de Informação (Bacharelado)</li>
-            <li>Sistemas para Internet (Tecnólogo)</li>
-        </ul>
-
-        <p>Caso queira conhecer os cursos, entre em contato: mschuster@salesiano.br</p>
-
-        <h4>Nossos Projetos (Texto inicial)</h4>
-
-        <ul>
-            <li>
-                Website - O nosso Website foi produzido "em casa", aqui você encontrará informações de como ele foi conduzido.
-                Caso queira apoio para fazer o seu, ou informações sobre este trabalho, só entrar em contato!
-            </li>
-            <li>
-                BioIndica - Projeto que está sendo conduzido de forma intercurso - Cursos de Tecnologia da Informação e das Ciências Biológicas
-                para um cliente especial o IEMA. O Sistema consiste em um apoio tecnológico para monitoramento e acompanhamento de espécimes no
-                parque Paulo César Vinha - importante monitoramento do meio ambiente conduzido de forma aplicada neste interessante estudo aplicado.
-                Mais informações sobre o projeto, só entrar em contato!
-            </li>
-            <li>
-                Software Bicicletário - Software livre para controle de acesso a bicicletários. Contribuição do Unitech para ambientes que precisam deste controle.
-                </li>
-            <li>
-                Fliperama - Trabalho interdisciplinar sendo conduzido para testes utilizando arduíno, raspberry e programação.
-                Vai ajudar na aplicação de técnicas e conceitos aprendidos em sala de aula... e para diversão!
-            </li>
-            <li>
-                Quiz acadêmico - uma contribuição do Unitech para o curso de Psicologia, para aumentar o engajamento em eventos que participam e para uso em sala de aula.
-            </li>
-        </ul>
+        <h2>Em Produção</h2>
     </section>
 </main>
 @include('template.footer')
