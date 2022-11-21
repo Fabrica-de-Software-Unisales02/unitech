a285c3cb5fcb1f2e8a5d4d483527105432ca54e0
izaelgs <izael-123@outlook.com>
2022-11-21 19:17:08 -0300

    aaaaaaaaaaaaaaaaaaaa

--- resources/views/template/footer.blade.php
+++ resources/views/template/footer.blade.php
@@ -11,8 +11,8 @@
     </section>
 </footer>
 </body>
-<!-- <script src='{{ url(mix("js/jquery.js")) }}'></script>
-<script src='{{ url(mix("js/bootstrap.js")) }}'></script> -->
+<script src='{{ url(mix("js/jquery.js")) }}'></script>
+<!-- <script src='{{ url(mix("js/bootstrap.js")) }}'></script> -->
 <script src='{{ url(mix("js/app.js")) }}'></script>
 @if($pagina)
     <script src='{{ url(mix("js/$pagina.js")) }}'></script>
